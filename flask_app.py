609d15506e11161c2484ad67eca10d9037d400f8
accrfc <accrfc@gmail.com>
2025-05-06 11:38:34 +0100

    Simplify flask_app.py for Render deployment

--- flask_app.py
+++ flask_app.py
@@ -1,10 +1,4 @@
-import sys
-import os
+from app import app
 
-# Add the current directory to the Python path
-current_dir = os.path.dirname(os.path.abspath(__file__))
-if current_dir not in sys.path:
-    sys.path.append(current_dir)
-
-# Import your Flask app
-from app import app as application 
+# This is the variable that Gunicorn looks for
+application = app 
